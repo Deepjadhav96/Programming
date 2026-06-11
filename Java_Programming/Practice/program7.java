78210ba1e92d9e4340559e87e956f939786e42ce
Deepraj <deepjadhav183@gmail.com>
2026-06-12 00:40:28 +0530

    Print even numbers

---
+++ Java_Programming/Practice/program7.java
@@ -0,0 +1,25 @@
+class Logic
+{
+    void printEvenNumbers(int iNum)
+    {
+        int iCnt = 0;
+        for(iCnt = 1; iCnt < iNum; iCnt++)
+        {
+            if(iCnt % 2 == 0 )
+            {
+                System.out.println(iCnt);
+            }
+        }
+    }
+}
+
+
+class program7
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.printEvenNumbers(11);
+
+    }
+}
