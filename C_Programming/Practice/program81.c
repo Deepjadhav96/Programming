19c0b438b67d849c800e8156c8fd3763a7b30097
Deepraj <deepjadhav183@gmail.com>
2026-06-13 23:38:54 +0530

    Non Sequential pattern

---
+++ C_Programming/Practice/program81.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+void pattern(int iRow, int iColumn)
+{
+    int iCntR  = 0 , iCntC = 0 ;
+    for(iCntR = 1; iCntR <= iRow; iCntR++ )
+    {
+        for(iCntC = 1; iCntC <= iColumn; iCntC++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+
+    }
+    
+    printf("\n");
+
+}
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+
+    printf("Enter number of elements:");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of elements:");
+    scanf("%d",&iValue2);
+
+
+    pattern(iValue1, iValue2);
+    return 0;
+}
+
+/////////////////////////////////////////
+////// Input: iRow = 4 iCol = 3
+//////   *  *  * 
+//////   *  *  * 
+//////   *  *  * 
+//////   *  *  * 
+//////
+////////////////////////////////////////
