20835615642a1043ab3b64ed51a112c0a44a34db
Deepraj <deepjadhav183@gmail.com>
2026-06-15 11:41:40 +0530

    Non sequential pattern 10

---
+++ C_Programming/Practice/program90.c
@@ -0,0 +1,41 @@
+/*
+Enter number of rows:3
+Enter number of columns:4
+1	2	3	4	
+5	6	7	8	
+9	10	11	12
+*/
+
+#include<stdio.h>
+
+void pattern(int iRow, int iColumn)
+{
+    int i  = 0 , j = 0 ,counter = 1 ;
+    
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iColumn; j++)
+        {
+          printf("%d\t",counter);
+          counter++;
+        }
+        printf("\n");
+    }
+
+    
+}
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+
+    printf("Enter number of rows:");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns:");
+    scanf("%d",&iValue2);
+
+
+    pattern(iValue1, iValue2);
+    return 0;
+}
