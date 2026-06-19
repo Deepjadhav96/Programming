1ef536d8d81d610843deda2cf367cbaf8a52fd60
Deepraj <deepjadhav183@gmail.com>
2026-06-19 09:52:10 +0530

    Number pattern printing

---
+++ C_Programming/Practice/program105.c
@@ -0,0 +1,59 @@
+/*
+Enter number of rows: 4
+Enter number of columns: 4 
+1	2	3	4	
+ 	2	3	4	
+ 	 	3	4	
+ 	 	 	4
+
+*/
+
+
+#include<stdio.h>
+
+void Display(int iRow, int iColumn)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iColumn)
+    {
+        printf("Invalid input");
+        printf("Number of row and column should be same");
+        return;
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j<= iColumn; j++)
+        {
+            if(i == j || i == 1 || j== iColumn ||(i-j == -1))
+            {
+                printf("%d\t",j);
+            }
+           else
+           {
+             printf(" \t");
+
+           }
+            
+        }
+        printf("\n");
+    }
+
+
+}
+
+
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns: ");
+    scanf("%d",&iValue2);
+
+    Display(iValue1 , iValue2);
+    return 0;
+}
