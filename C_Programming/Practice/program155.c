7e4f3da8ed9204fb1f57692a163a78e516022b0e
Deepraj <deepjadhav183@gmail.com>
2026-07-01 23:16:29 +0530

    Modified number after toggle bit

---
+++ C_Programming/Practice/program155.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+UINT ToggleBit(UINT iNo, UINT iMask)
+{
+    UINT iAns = 0;
+
+  
+    iAns = iNo | iMask;
+
+    return iAns;
+
+
+}
+
+int main()
+{
+    UINT iValue = 0;
+    UINT iMask = 0XF;
+    UINT iResult = 0;
+
+    printf("Enter a Number:");
+    scanf("%d",&iValue);
+
+    iResult =ToggleBit(iValue, iMask);
+    printf("Modified number:%d\n ",iResult);
+
+    return 0;
+}
