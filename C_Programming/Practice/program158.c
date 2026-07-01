9806609ea8a221c23ba5232748215ce0e3188f7b
Deepraj <deepjadhav183@gmail.com>
2026-07-01 23:22:43 +0530

    Check bit is on or off

---
+++ C_Programming/Practice/program158.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+UINT ChkBit(UINT iNo, UINT iPos)
+{
+    UINT iMask = 0x1;
+    UINT Output = 0;
+
+    iMask = iMask << (iPos -1);
+
+  
+
+     Output = iNo | iMask;
+
+    return Output;
+
+}
+
+int main()
+{
+    UINT iValue = 0;
+    UINT iLocation = 0;
+    UINT iResult = 0;
+
+    printf("Enter a Number:");
+    scanf("%d",&iValue);
+
+    printf("Enter a position:");
+    scanf("%d",&iLocation);
+
+    iResult =ChkBit(iValue,iLocation);
+    printf("Number after Bit on %d\n",iResult);
+    
+    return 0;
+}
