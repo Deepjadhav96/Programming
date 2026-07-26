52fd6bee2b65184b810212519a8a2c4b6a41c710
Deepraj <deepjadhav183@gmail.com>
2026-07-26 23:47:27 +0530

    Traversing the directory in Python

---
+++ Python_Programming/Python_Automation/DirectoryTraversal2.py
@@ -0,0 +1,10 @@
+import os
+
+def main():
+    for FolderName, SubFolder, FileName in os.walk("Marvellous"):
+        print("Folder Name:",FolderName)
+        for subf in SubFolder:
+            print("Subfolder name: ",subf)
+            
+if __name__ == "__main__":
+    main()
