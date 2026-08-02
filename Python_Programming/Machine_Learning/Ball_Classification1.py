76341cb24fa98b724215a1e91ff9179fe04f6bb3
Deepraj <deepjadhav183@gmail.com>
2026-08-02 23:58:11 +0530

    Machine learning case study

---
+++ Python_Programming/Machine_Learning/Ball_Classification1.py
@@ -0,0 +1,12 @@
+def main():
+    print("Ball classification case study")
+
+    Features = [[35,"Rough"],[47,"Rough"],[90,"Smooth"],[48,"Rough"],[90,"Smooth"],[35,"Rough"],[92,"Smooth"],[35,"Rough"],[35,"Rough"],[35,"Rough"],[96,"Smooth"],[43,"Rough"],[110,"Smooth"],[35,"Rough"],]
+
+    Labels = ["Tennis","Tennis","Cricket","Tennis","Cricket","Tennis","Cricket","Tennis","Tennis","Tennis","Cricket","Tennis","Cricket","Cricket","Tennis","Cricket"]
+
+    print("Features : ",Features)
+    print("Labels : ",Labels)
+    
+if __name__ == "__main__":
+    main()
