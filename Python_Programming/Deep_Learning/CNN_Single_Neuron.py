400d667abf689850ebe535059701e95a73910bd8
Deepraj <deepjadhav183@gmail.com>
2026-09-19 23:48:26 +0530

    CNN singlr neuron

---
+++ Python_Programming/Deep_Learning/CNN_Single_Neuron.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+inputs = tf.constant([1.0,2.0,3.0])
+
+weights = tf.constant([0.5,-0.2,0.8])
+
+bias = tf.constant(0.1)
+
+weighted_sum = tf.reduce_sum(inputs * weights) + bias
+
+print("Input : ",inputs.numpy())
+print("Weight : ",weights.numpy())
+print("Bias : ",bias.numpy())
+
+print("Weighted sum : ",weighted_sum.numpy())
+
+output = tf.sigmoid(weighted_sum)
+print("Output is :",output.numpy())
